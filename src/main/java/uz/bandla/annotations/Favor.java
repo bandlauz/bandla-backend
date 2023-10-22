6f6542e953af88f7508da6454576b0327d98475f
Azimjon Nazarov <109890132+nazarovv2@users.noreply.github.com>
2023-10-22 19:03:09 +0500

    Favor annotation created

---
+++ src/main/java/uz/bandla/annotations/Favor.java
@@ -0,0 +1,12 @@
+package uz.bandla.annotations;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Component
+public @interface Favor {
+}
