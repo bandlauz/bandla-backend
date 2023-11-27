93634e30f91ab53ba5370e259bfad23ad12e8ac3
Azimjon Nazarov <109890132+nazarovv2@users.noreply.github.com>
2023-11-27 22:19:24 +0500

    Exception handled

--- src/main/java/uz/bandla/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/uz/bandla/security/jwt/JwtAuthenticationFilter.java
@@ -1,13 +1,15 @@
 package uz.bandla.security.jwt;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import uz.bandla.exp.auth.ProfileLockedException;
+import uz.bandla.exp.auth.TokenExpiredException;
 import uz.bandla.security.profile.ProfileDetailsService;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,18 +17,24 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Component
-@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
+
     private final ProfileDetailsService userDetailsService;
+
+    @Qualifier("handlerExceptionResolver")
+    @Autowired
+    private HandlerExceptionResolver exceptionResolver;
+
+    public JwtAuthenticationFilter(JwtService jwtService, ProfileDetailsService userDetailsService) {
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+    }
 
     @Override
     protected void doFilterInternal(
@@ -43,8 +51,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         final String accessToken = authHeader.substring(7);
 
         if (jwtService.isTokenExpired(accessToken)) {
-            String tokenExpiredMessage = jwtService.getTokenExpiredMessage(accessToken);
-            writeError(response, tokenExpiredMessage);
+            String message = jwtService.getTokenExpiredMessage(accessToken);
+            exceptionResolver.resolveException(request, response, null, new TokenExpiredException(message));
             return;
         }
 
@@ -52,12 +60,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 
-            if (!userDetails.isAccountNonLocked()) {
-                writeError(response, "User account is locked");
-                return;
-            }
-            if (!userDetails.isEnabled()) {
-                writeError(response, "User is disabled");
+            if (!userDetails.isAccountNonLocked() || !userDetails.isEnabled()) {
+                exceptionResolver.resolveException(request, response, null, new ProfileLockedException());
                 return;
             }
 
@@ -75,18 +79,5 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) {
         return request.getServletPath().equals("/auth/refresh-token");
-    }
-
-    private void writeError(HttpServletResponse response, String message) {
-        response.setHeader("error", message);
-        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-        Map<String, String> error = new HashMap<>();
-        error.put("error", message);
-        response.setContentType(APPLICATION_JSON_VALUE);
-        try {
-            new ObjectMapper().writeValue(response.getOutputStream(), error);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
