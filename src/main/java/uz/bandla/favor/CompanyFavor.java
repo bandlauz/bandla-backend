c73961516179172b6dded8de2454eea3da861d57
Azimjon Nazarov <109890132+nazarovv2@users.noreply.github.com>
2023-11-28 22:09:30 +0500

    Company added

---
+++ src/main/java/uz/bandla/favor/CompanyFavor.java
@@ -0,0 +1,4 @@
+package uz.bandla.favor;
+
+public interface CompanyFavor {
+}
---
+++ src/main/java/uz/bandla/favor/impl/CompanyFavorImpl.java
@@ -0,0 +1,8 @@
+package uz.bandla.favor.impl;
+
+import uz.bandla.annotations.Favor;
+import uz.bandla.favor.CompanyFavor;
+
+@Favor
+public class CompanyFavorImpl implements CompanyFavor {
+}
