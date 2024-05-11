337f6f7b767ad5cf810b1011c7bc46e13386f307
Azimjon Nazarov <109890132+nazarovv2@users.noreply.github.com>
2024-05-11 19:33:30 +0500

    Query replaced

--- src/main/java/uz/bandla/repository/NonceRepository.java
+++ src/main/java/uz/bandla/repository/NonceRepository.java
@@ -3,6 +3,16 @@ package uz.bandla.repository;
 import uz.bandla.entity.NonceEntity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import uz.bandla.exp.NotFoundException;
+
+import java.util.Optional;
 
 public interface NonceRepository extends JpaRepository<NonceEntity, String> {
+    default NonceEntity findByIdOrElseThrow(String id) {
+        Optional<NonceEntity> optional = findById(id);
+        if (optional.isEmpty()) {
+            throw new NotFoundException("Nonce not found");
+        }
+        return optional.get();
+    }
 }
